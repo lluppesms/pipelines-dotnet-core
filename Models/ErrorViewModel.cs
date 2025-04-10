0b1cf59332698bd252414a8def8b7031095ee3eb
Lab Luppes <labs@luppes.com>
2025-04-10 08:40:50 -0500

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pipelines_dotnet_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
